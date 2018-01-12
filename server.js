9db8fb32a7434f2ac1db5ed5b65d089e8292e4e0
Ben <bgardner620@gmail.com>
2018-01-12 15:14:51 -0500

    new

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
-require('babel-register');
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 
-require('./server.babel');
+let app = express()
+
+app.use(bodyParser.json())
+
+app.post('/api/send', (req, res) => {
+  let SID = process.env.TWILIO_SID;
+  let TOKEN = process.env.TWILIO_TOKEN;
+  let SENDER = process.env.TWILIO_SENDER
+
+  if(!SID || !TOKEN) {
+    return res.json({message: 'add TWILIO_SID and TWILIO_TOKEN to .env file.'})
+  }
+
+  let client = require('twilio')(SID, TOKEN)
+
+  client.sendMessage({
+    to: req.body.recipient,
+    from: SENDER
+    body: 'word to your mother.'
+  }, (err, responseData) => {
+    if (!err) {
+      res.json({
+        From: responseData.from, 
+        Body: responseData.body
+      })
+    }
+  })
+})
+
+app.listen(3000)
--- src/client.js
+++ src/client.js
@@ -9,29 +9,37 @@ class App extends React.Component {
       recipient: ''
     };
   }
-  changeInput(event) {
+
+  changeInput = (event) => {
     this.setState({recipient: event.target.value});
   }
 
-  sendSms() {
-    fetch('/sendsms', {
+  sendSms = () => {
+    fetch('/api/send', {
       method: 'POST',
       headers: {
         Accept: 'application/JSON',
         'Content-Type': 'application/JSON'
       },
-      body: JSON.stringify({"recipient": this.state.recipient})
-    });
+      body: JSON.stringify({recipient: this.state.recipient})
+    })
+    .then(resp => resp.json())
+    .then(resp => {
+      console.log(resp)
+    })
   }
+
   render() {
     return (
       <div>
         <p>Enter phone number to send SMS to: </p>
-      <input onChange={this.changeInput.bind(this)} value={this.state.recipient} placeholder="+12223334444"/>
-        <button onClick={this
-          .sendSms
-          .bind(this)}>Send message</button>
-          <p>Don't forget your country code, e.g., +1.</p>
+        <input 
+          onChange={this.changeInput} 
+          value={this.state.recipient} 
+          placeholder="+12223334444"
+        />
+        <button onClick={this.sendSms}>Send message</button>
+        <p>Don't forget your country code, e.g., +1 in the US.</p>
       </div>
     );
   }
